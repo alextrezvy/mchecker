db1ae86374415187953b1e2f0cba74aebdc8975e
user <alextrezvy@gmail.com>
2018-03-02 14:38:44 +0500

    Added cmd file for Windows

---
+++ start.cmd
@@ -0,0 +1,16 @@
+rem Checks miner in a loop
+@echo off
+set PROFILE=xmrstak
+
+
+:check
+echo "Checking miner..."
+java -cp .\mchecker-0.0.4.jar;.\dependency\* -Dspring.profiles.active=%PROFILE% -Djava.util.logging.config.file=logging.properties my.app.mchecker.MChecker
+if %errorlevel% neq 0 (
+  echo "Miner error, rebooting..."
+  shutdown /r
+) else (
+  echo "Miner OK, sleeping..."
+  choice /c a /d a /t 60
+)
+goto check
